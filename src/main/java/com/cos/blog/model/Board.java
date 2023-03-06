77f2d99fbf1b2e1bd0b1a1f884ca88b0353d506f
sunghyun1999 <32184099@dankook.ac.kr>
2023-03-06 18:20:43 +0900

    User, Board, Reply 테이블 생성 및 연관관계 매핑

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,43 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne(fetch = FetchType.EAGER) // Board = Many, User = One
+    @JoinColumn(name = "userId")
+    private User user; // DB는 오브젝트를 저장할 수 없다. 자바는 오브젝트를 저장할 수 있다.
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy: 연관관계의 주인이 아니므로, DB에 컬럼을 만들지 않는다.
+    private List<Reply> reply;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
