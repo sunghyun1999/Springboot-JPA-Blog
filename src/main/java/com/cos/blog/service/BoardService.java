dc0ec40acd9a35eeabf41e859a7b4c019e17f28a
sunghyun1999 <32184099@dankook.ac.kr>
2023-03-16 19:29:21 +0900

    글수정 및 글삭제 기능 구현

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -4,6 +4,8 @@ import com.cos.blog.model.Board;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +24,32 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    public List<Board> 글목록() {
-        return boardRepository.findAll();
+    @Transactional(readOnly = true)
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Board 글상세보기(int id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+                });
+    }
+
+    @Transactional
+    public void 글삭제하기(int id) {
+        boardRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void 글수정하기(int id, Board requestBoard) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalArgumentException("글 찾기 실패 : 해당 글을 찾을 수 없습니다.");
+                }); // 영속화 완료
+
+        board.setTitle(requestBoard.getTitle());
+        board.setContent(requestBoard.getContent());
     }
 }
